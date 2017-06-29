0f52384ec4370caeb21b608a40784532692405d8
raminfp <ramin.blackhat@gmail.com>
2017-06-29 12:39:04 +0430

    Fixed config.status: error: cannot find input file: 'Makefile.in'

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-autoreconf --force --install
-rm -rf autom4te*.cache
+touch AUTHORS NEWS ChangeLog README
+autoheader \
+    && aclocal \
+    && libtoolize --ltdl --copy --force \
+    && automake --add-missing --copy \
+    && autoreconf --force --install 
+    rm -rf autom4te*.cache
